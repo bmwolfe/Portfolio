c498c9e532cea96f70ac585ac2d5bc7bc98505ae
Brendon Wolfe <114600119+bmwolfe@users.noreply.github.com>
2024-09-08 02:44:32 -0400

    Commented out placeholders in projects.js

--- data/projects.js
+++ data/projects.js
@@ -7,34 +7,34 @@ export function getProjectsTotal(projects){
 }
 
 export const projects = [
-    {
-        name: "Finite Automata Minecraft Mod",
-        description: "This project was the 2nd place winner for the Grizzhacks6 Hackathon in 2024",
-        thumbnail: "",
-        video: "",
-        inprogress: false
-     }, 
-     {
-         name: "Youtube Clone",
-         description: "Here I worked on my HTML and CSS skills.",
-         thumbnail: "",
-         video: "",
-         inprogress: true
-     },
-     {
-         name: "Universal Password Website",
-         description: "The is an HTML and JavaScript project meant to be used with other websites",
-         image: "",
-         video: "",
-         inprogress: true
-     }, 
-     {
-         name: "Arcade Games (Group)",
-         description: "This project was a group Java project where I worked with Java and JavaFX",
-         image: "",
-         video: "",
-         inprogress: false
-     }//,
+//     {
+//         name: "Finite Automata Minecraft Mod",
+//         description: "This project was the 2nd place winner for the Grizzhacks6 Hackathon in 2024",
+//         thumbnail: "",
+//         video: "",
+//         inprogress: false
+//      }, 
+//      {
+//          name: "Youtube Clone",
+//          description: "Here I worked on my HTML and CSS skills.",
+//          thumbnail: "",
+//          video: "",
+//          inprogress: true
+//      },
+//      {
+//          name: "Universal Password Website",
+//          description: "The is an HTML and JavaScript project meant to be used with other websites",
+//          image: "",
+//          video: "",
+//          inprogress: true
+//      }, 
+//      {
+//          name: "Arcade Games (Group)",
+//          description: "This project was a group Java project where I worked with Java and JavaFX",
+//          image: "",
+//          video: "",
+//          inprogress: false
+//      }//,
     // {
     //     name: "Amazon Clone",
     //     description: "hello",
